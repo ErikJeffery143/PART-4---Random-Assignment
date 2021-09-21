5e0c1ba35bf9ddc50cd12052ea8458a9d210708f
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-09-21 12:18:17 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PART_4___Random_Assingment
+{
+    partial class RandomNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInteger = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInteger
+            // 
+            this.btnInteger.Location = new System.Drawing.Point(64, 198);
+            this.btnInteger.Name = "btnInteger";
+            this.btnInteger.Size = new System.Drawing.Size(105, 23);
+            this.btnInteger.TabIndex = 0;
+            this.btnInteger.Text = "Get Your Integer";
+            this.btnInteger.UseVisualStyleBackColor = true;
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(209, 198);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(101, 23);
+            this.btnDouble.TabIndex = 1;
+            this.btnDouble.Text = "Get Your Double";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(12, 60);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(255, 13);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "Please enter your MINIMUM value";
+            this.lblMin.Click += new System.EventHandler(this.lblMin_Click);
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(12, 104);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(223, 13);
+            this.lblMax.TabIndex = 5;
+            this.lblMax.Text = "Please enter your MAX value";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(64, 243);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(16, 13);
+            this.lblFinal.TabIndex = 6;
+            this.lblFinal.Text = "...";
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(116, 77);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(120, 20);
+            this.numMin.TabIndex = 7;
+            // 
+            // numMax
+            // 
+            this.numMax.Location = new System.Drawing.Point(116, 121);
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(120, 20);
+            this.numMax.TabIndex = 8;
+            // 
+            // RandomNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 314);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInteger);
+            this.Name = "RandomNumbers";
+            this.Text = "Random Numbers";
+            this.Load += new System.EventHandler(this.RandomNumbers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInteger;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numMax;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PART_4___Random_Assingment
+{
+    public partial class RandomNumbers : Form
+    {
+        Random generator = new Random();
+        int Min;
+        int Max;
+        int randNum;
+
+
+        public RandomNumbers()
+        {
+            InitializeComponent();
+        }
+        
+        private void RandomNumbers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            
+
+            randNum = generator.Next(Min, Max);
+           
+            
+            lblFinal.Text = randNum + "";
+
+
+
+
+
+
+        }
+
+        private void lblMin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
